6e4c599f5f32bbcfff41ce6d4be4d12d384b5f39
mrgonzzo <mr.gonzzo@gmail.com>
2017-07-25 11:33:03 +0200

    OK ver 1.0.1 sound bug on 'play again' solved :)

--- README.md
+++ README.md
@@ -4,8 +4,3 @@ It's a simple memory game implemented with [AngularJS](https://angularjs.org/ "A
 You can send coments to <mr.gonzzo@gmail.com> and download de apk file for test from:  
  
 https://drive.google.com/open?id=0B5VfUqfE2ujqeWRhRDhUd1RzMVk
-
-
-
-
-
--- src/app/components/play/deck/deck.js
+++ src/app/components/play/deck/deck.js
@@ -4,11 +4,11 @@
         // otra opción es poner aquí directamente el html asignándoselo a template:
         templateUrl: 'app/components/play/deck/deck.html',
         // en controller definimos la función que escribimos abajo
-        controller: ['$state', '$timeout', 'scoreFactory', 'cardFactory', 'soundFactory', controllerCompDeck],
+        controller: ['$state','$scope', '$timeout', 'scoreFactory', 'cardFactory', 'soundFactory', controllerCompDeck],
         // declaramos un alias para no tener que usar $ctrl.
         controllerAs: 'compDeck'
     });
-    function controllerCompDeck($state, $timeout, scoreFactory, cardFactory, soundFactory) {
+    function controllerCompDeck($state,$scope, $timeout, scoreFactory, cardFactory, soundFactory) {
         var vm = this;
         var dckturn = cardFactory.getTurn();
 
@@ -19,16 +19,20 @@
                     deck[i][j].stat = 'backed';
                 }
             }
+            soundFactory.playClocksound();
             return deck;
         };
 
         vm.$onInit = function () {
             vm.deck = angular.copy(cardFactory.getDeck());
-            $timeout(function () {
+            timer = $timeout(function () {
                 vm.deck = vm.flop(vm.deck);
                 dckturn.length = 0;
-                soundFactory.playClocksound();
+
             }, 6000);
+            $scope.$on('$destroy', function () {
+                $timeout.cancel(timer);
+            });
         };
     }
 })(angular)
